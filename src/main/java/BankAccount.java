b4049873c8a5ba71dc380df8d560d79dfcc1128d
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-15 14:57:57 +0000

    Added getters and setters, Runner file

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -24,6 +24,51 @@ public class BankAccount {
 
 
     // Getters and Setters:
+    // _______ firstName _______
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    // ________ lastName _________
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    // _______ dateOfBirth ________
+    public LocalDate getDateOfBirth(){
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // _______ accountNumber _______
+    public Long getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // ________ balance _______
+    public Long getBalance() {
+        return this.balance;
+    }
+
+
+    public void setBalance(Long balance){
+        this.balance = balance;
+    }
 
 
 
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,7 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
+
 }
